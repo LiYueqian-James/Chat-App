890bcc3894efc5129f76e73eb1a891d66aa72a18
James Li <yl176@rice.edu>
2021-11-07 13:30:11 -0600

    adptr doc string

--- src/hpn2_yl176/mini_mvc/model/IModel2ViewAdptr.java
+++ src/hpn2_yl176/mini_mvc/model/IModel2ViewAdptr.java
@@ -9,8 +9,15 @@ package hpn2_yl176.mini_mvc.model;
  */
 public interface IModel2ViewAdptr {
 	
+	/**
+	 * @param msg the message to be displayed
+	 */
 	public void displayMsg(String msg);
 	
+	
+	/**
+	 * @param status the status messsage to be displayed
+	 */
 	public void displayStatus(String status);
 	
 
