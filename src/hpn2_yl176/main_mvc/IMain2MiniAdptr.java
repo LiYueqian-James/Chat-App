16f319bfe5e0ddfd1da2ab8b9380f36014dc0ec1
James Li <yl176@rice.edu>
2021-11-15 00:13:54 -0600

    main2mini

--- src/hpn2_yl176/main_mvc/IMain2MiniAdptr.java
+++ src/hpn2_yl176/main_mvc/IMain2MiniAdptr.java
@@ -9,13 +9,14 @@ import common.receiver.INamedReceiver;
 
 /**
  * @author James Li
- * Main wants the mini to...
+ * The mini model doesn't know if the room is created by the current chat app instance
+ * or if it is created via invitation.
  */
 public interface IMain2MiniAdptr {	
 	/**
 	 * @return the named receiver representing the stub of the chat room(the mini mvc)
 	 */
-	public INamedReceiver makeNamedReceiver();
+	public INamedReceiver getNamedReceiver();
 	
 	/**
 	 * @return the panel containing the view of a chat room.
